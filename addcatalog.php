591fc1773ec9abedf61b55dcb1729e956e67d19a
EkaterinaDankovtceva <dankovtseva.ekaterina@yandex.ru>
2016-12-26 00:41:23 +0300

    Update addcatalog.php

--- addcatalog.php
+++ addcatalog.php
@@ -12,26 +12,33 @@ table
  width: 40%; 
  font-size:20pt;
  margin: auto;
- 
+ top: 40%;
  padding: 4px;
  text-align: center;
 }
+body{ 
+background-image:url(logo.png); 
+background-repeat: no-repeat; 
+background-position: 920px 550px; 
+}
+
 </style>
 </head>
 <body>
 <form method="post">
-<button type="submit" name="back_button" align="left"  style="width:90;height:25; margin-left:20px;margin-top:5px;" ><a href='catalogs.php'>Back</a></button>
-<button type="submit" name="add_button" align="left"  style="width:90;height:25; margin-left:100px;margin-top:50px;" >Add</button>
+
 <div class="main">
 <?
 $isAdmin=$_GET["isAdmin"];?>
 <table>
 <tr>
-<td>Id_catalog:</td>
+<td align="right">Id_catalog:</td>
  <td><input type="text" name="adding1" maxlength="4" size="20"></td></tr>
 <tr>
-<td>Name_catalog:</td>
- <td><input type="text" name="adding2" maxlength="4" size="20"></td></tr>
+<td align="right">Name_catalog:</td>
+ <td><input type="text" name="adding2"  size="20"></td>
+  <td><button type="submit" name="add_button" align="left" style="width:90;height:25; left:20pt; top : 20pt;">Add</button></td>
+ </tr>
 </table>
 <?php 
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
@@ -40,7 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	 $add_id=$_POST['adding1'];
 	 $add_name=$_POST['adding2'];
      
-	 if(is_integer($adding_id)& $add_name!=null)
+	 if(is_integer($add_id)& $add_name!=null)
 	{
 		require("connect.php");
 		$sql = "INSERT INTO catalog_tour(id_catalog,nameCatalog) VALUES('$add_id','$add_name')";
@@ -48,10 +55,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 		mysqli_close($conn);
 		echo "<meta http-equiv=refresh content=0>";
 	}
- }
- }
- else 
+	else 
  echo"<font color='red' size=3pt>Incorrect data!</font>";
+ }
+ }
+ 
 
 
 
